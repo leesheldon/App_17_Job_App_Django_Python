c986c4b2c059057673b4059bb315693a4794d705
Sheldon <lhanco@gmail.com>
2024-04-03 22:45:29 +0700

    Day 53: Get the user data from Application form.

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            start_date = form.cleaned_data["start_date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name)
+            print(last_name)
+            print(email)
+            print(start_date)
+            print(occupation)
     return render(request, "index.html")
 
