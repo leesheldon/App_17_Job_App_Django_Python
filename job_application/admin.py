fa9a2d9d8d8e2e6e19f319a5014b8c4655f3d318
Sheldon <lhanco@gmail.com>
2024-04-04 12:24:43 +0700

    Day 54: Customize the Admin interface.

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("start_date", "occupation")
+    # minus means reverse ordering (Z -> A)
+    ordering = ("-first_name", )
+    readonly_fields = ("occupation", )
+
+
+admin.site.register(Form, FormAdmin)
